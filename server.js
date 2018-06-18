87cd6f800b1a1d4522ade3e41436d0773943d2cf
nturpin <nova.t974@gmail.com>
2018-06-18 16:10:32 +0400

    Affichage du résultat qui ne ne s'affiche pas

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
 var express = require('express'),
     server = express();
 var bodyParser = require('body-parser');
-var messages = [];
+//var messages = [];
 
-server.use(express.static(__dirname));
+server.use(express.static("static"));
 
-server.use(bodyParser.urlencoded({extended:false}));
+server.use(bodyParser.urlencoded({extended:true}));
 
 server.get("/",function(req, res){
     res.sendFile(__dirname +"/index.html");
 });
 
-server.post("/",function(req, res){
-    console.log(req.body);
+// server.get("/",function(req, res){
+//     console.log(req.body);
+//     messages.push({email: req.body.email, message: req.body.message});
+//     //res.sendFile(__dirname +"/index.html");
+// });
+
+
+server.post("/addition",function(req, res){
+    //console.log(req.body);
+    var nb1 = parseInt(req.body.nb1);
+    console.log(nb1)
+    var nb2 = parseInt(req.body.nb2);
+    console.log(nb2)
+    var total = nb1 + nb2;
+    console.log(total);
+    // console.log(total);
+    //res.send("{"+total+"}");
+    //res.writeHead(200);
+    // res.setHeader('Content-Type', 'application/json');
+    // res.send(JSON.stringify({ resultat: total }));
+    res.send(''+total);
+    // messages.push({email: req.body.email, message: req.body.message});
+    //res.sendFile(__dirname +"/index.html");
+
+   
 });
 
-server.listen(3002);    
+
+server.listen(3105);    
---
+++ static/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {        
+    $('#btn').click(function () {
+        var nb1 = $("#nb1").val();
+        var nb2 = $("#nb2").val();
+        $.post('http://localhost:3105/addition', {
+            nb1: nb1, 
+            nb2: nb2
+        },  function(data){
+            console.log(data);
+            $("#resultat").append(data);
+            }
+        )
+
+    });  
+});
+    
+        /***** ******** */
+        //console.log("click");
+        // $.ajax({
+        //     type: 'POST',
+        //     url: 'http://localhost:3105/addition',
+        //     data :'nb1=' + nb1 + '&nb2=' +nb2 ,
+        //     success: function (calcul) {
+        //         console.log(calcul);
+        //         var result = "";
+
+        //         // for (var i = 0; i < calcul.length; i++) {
+        //         //     result = calcul.total;
+        //         //     console.log(result);
+        //         //     return nb1 + nb2;
+        //         // }
+        //         $("#resultat").append('' + calcul.total + '');
+        //     },
+        //     error: function (resultat, statut, erreur) {
+
+        //         //alert('ERROR 404');
+        //         console.log("error requête Ajax");
+        //         //console.log(resultat);
+        //     }
+        // });
+        
+
+
+
