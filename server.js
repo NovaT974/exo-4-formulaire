cc66257ea0b841a48724c15e60835c2cbd8e449b
nturpin <nova.t974@gmail.com>
2018-06-18 10:14:49 +0400

    recupération des données du formulaire

--- server.js
+++ server.js
@@ -1,8 +1,18 @@
 var express = require('express'),
     server = express();
-
-    var messages = [];
+var bodyParser = require('body-parser');
+var messages = [];
 
 server.use(express.static(__dirname));
 
-server.listen(8081);    
+server.use(bodyParser.urlencoded({extended:false}));
+
+server.get("/",function(req, res){
+    res.sendFile(__dirname +"/index.html");
+});
+
+server.post("/",function(req, res){
+    console.log(req.body);
+});
+
+server.listen(3002);    
